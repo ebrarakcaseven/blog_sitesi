49304882d9050f7b568944c5408d3b155dd51160
Ebrar Akçaseven <332361004@ogr.uludag.edu.tr>
2025-01-01 15:42:49 +0300

    ebilog

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,36 +15,96 @@ public class HomeController : Controller
     {
         _db = db;
     }
-public IActionResult Index()
-{
-    var makale = _db.Makales
-        .Where(i => i.Onay == true)
-        .Select(i => new MakaleModel()
-        {
-            Id = i.Id,
-            Baslik = i.Baslik,
-            KullaniciAd = i.KullaniciAd,
-            YayinTarih = i.YayinTarih,
-            Onay = i.Onay,
-            Goruntulenme = i.Goruntulenme,
-            Aciklama = i.Aciklama.Length > 60 ? i.Aciklama.Substring(0, 60) + "[...]" : i.Aciklama,
-            Yorums = i.Yorums.Select(y => new YorumModel
+    public IActionResult Index()
+    {
+        var makale = _db.Makales
+            .Where(i => i.Onay == true)
+            .Select(i => new MakaleModel()
             {
-                // Yorumla ilgili gerekli alanlar burada
-                Id = y.Id,
-                YorumMetni = y.YorumMetni
-            }).ToList() // YorumModel'e dönüştürme
-        })
-        .ToList();
+                Id = i.Id,
+                Baslik = i.Baslik,
+                KullaniciAd = i.KullaniciAd,
+                YayinTarih = i.YayinTarih,
+                Onay = i.Onay,
+                Goruntulenme = i.Goruntulenme,
+                Aciklama = i.Aciklama.Length > 60 ? i.Aciklama.Substring(0, 60) + "[...]" : i.Aciklama,
+                Yorums = i.Yorums.Select(y => new YorumModel
+                {
+                    // Yorumla ilgili gerekli alanlar burada
+                    Id = y.Id,
+                    YorumMetni = y.YorumMetni
+                }).ToList() // YorumModel'e dönüştürme
+            })
+            .ToList();
+        var kategoriler = _db.Kategoris.ToList();
 
-    return View(makale);
-}
-
+        ViewBag.kategoriler = kategoriler;
+        return View(makale);
+    }
+    public ActionResult MakaleListesi(int? id)
+    {
+        var makale = _db.Makales.Where(i => i.Onay == true).AsQueryable();
+        if (id != null)
+        {
+            makale = makale.Where(İ => İ.KategoriId == id);
+        }
+        // Kategoriler listesini doldur
+        ViewBag.Kategoriler = _db.Kategoris.ToList();
+        return View(makale);
+    }
+    public ActionResult Ara(string deger)
+    {
+        var ara = _db.Makales.Where(i => i.Onay == true && i.Aciklama.Contains(deger)).ToList();
+        ViewBag.Kategoriler = _db.Kategoris.ToList();
+        return View(ara);
+    }
     public ActionResult Detay(int id)
     {
-        var makale=_db.Makales.Find(id);
-        ViewBag.makale=makale;
-        return View();
+        var makale = _db.Makales.Find(id);  // Makaleyi bul
+        if (makale == null)
+        {
+            return NotFound();  // Eğer makale bulunamazsa 404 döndür
+        }
+
+        // Makale ve onun yorumları
+        var yorumlar = _db.Yorums.Where(y => y.MakaleId == id).ToList();  // Bu makaleye ait yorumları al
+
+        // Veriyi View'a gönder
+        ViewBag.makale = makale;
+        ViewBag.Yorumlar = yorumlar;
+        ViewBag.Kategoriler = _db.Kategoris.ToList();  // Kategorileri ekleyelim
+
+        // Görüntülenme sayısını artır
+        makale.Goruntulenme += 1;
+        _db.SaveChanges();
+
+        // Boş bir Yorum modeli gönderiyoruz ki kullanıcı yeni bir yorum yazabilsin
+        return View("Detay", new Yorum() { MakaleId = id });
     }
+
+
+
+    public ActionResult YorumGonder(Yorum yorum)
+    {
+        if (string.IsNullOrWhiteSpace(yorum.YorumMetni))
+        {
+            // Yorum metni boş ise kullanıcıyı uyar
+            TempData["errorMessage"] = "Yorum metni boş olamaz!";
+            return RedirectToAction("Detay", new { id = yorum.MakaleId });
+        }
+
+        // Kullanıcı bilgilerini ekle
+        yorum.KullaniciId = User.Identity.Name;
+        yorum.YorumTarihi = DateTime.Now;
+
+        // Yorum veritabanına ekle
+        _db.Yorums.Add(yorum);
+        _db.SaveChanges();
+
+        // Yorum eklendikten sonra makale detayına yönlendir
+        return RedirectToAction("Detay", "Home", new { id = yorum.MakaleId });
+    }
+
+
 
 }
--- Models/Yorum.cs
+++ Models/Yorum.cs
@@ -6,14 +6,14 @@ using System.Web;
 namespace Blog.Models
 {
     public class Yorum
-{
-    public int Id { get; set; }
-    public string YorumMetni { get; set; }
-    public DateTime YorumTarihi { get; set; }
-
-    // Yorum bir makaleye aittir
-    public int? MakaleId { get; set; }
-    public Makale? Makale { get; set; }
-}
+    {
+        public int Id { get; set; }
+        public string YorumMetni { get; set; }
+        public DateTime YorumTarihi { get; set; }
+        public string KullaniciId { get; set; }
+        // Yorum bir makaleye aittir
+        public int? MakaleId { get; set; }
+        public Makale? Makale { get; set; }
+    }
 
 }
